73bbd61d378bcd63571c16a396ba1f60844f888e
Julien Rohou <julien.rohou@laposte.net>
2017-11-14 16:31:20 +0100

    fait

--- src/speakers/details/index.js
+++ src/speakers/details/index.js
@@ -1,5 +1,7 @@
 import TalkService from '../../common/talk.service';
 
+import SessionService from '../../common/session.service';
+
 import template from './speaker.html'
 
 export default class SpeakerDetail {
@@ -11,21 +13,37 @@ export default class SpeakerDetail {
 
   render() {
     const tabSpeakers = this.talkService.findAllSpeakers()
+    const sess = new sessionService()
     let speakersHTML = []
     let title
     let image
     let link = []
+    let description
     let presentation
 
     tabSpeakers.then((speakers) => {
       speakers.forEach(sp => {
         if(sp.id == this.id) {
           title = sp.lastname + " " + sp.firstname;
-          image = '<img src="./src/images/'+sp.image+'"alt="img_presentateur"/>';
+          image = '<img src="./src/images/'+sp.image+'"alt="img_presentateur" class="rounded mx-auto d-block" class="img-thumbnail"/>';
           sp.socials.forEach(soc => {
-            link.push('<li class="list-inline-item"><a href="'+soc.link+'">'+soc.class+'</a>');
+            if(soc.class=="google-plus") {
+              link.push('<li class="list-inline-item"><a href="'+soc.link+'"><img src="./src/speakers-img/google-logo.png" width="40px" class="rounded mx-auto d-block"></a>');
+            }
+            else if(soc.class=="twitter"){
+            link.push('<li class="list-inline-item"><a href="'+soc.link+'"><img src="./src/speakers-img/twitter.jpg" width="40px" class="rounded mx-auto d-block"></a>');
+          }
+            else if(soc.class=="github"){
+            link.push('<li class="list-inline-item"><a href="'+soc.link+'"><img src="./src/speakers-img/github-mark.png" width="40px" height="40px" class="rounded mx-auto d-block"></a>');
+          }
           })
-          presentation = sp.about
+          description = sp.about
+
+          sess.forEach(se =>{
+            sessions = se.speakers.filter(spik => {
+              spik == sp.id;
+            })
+          })
         }
       })
 
@@ -33,7 +51,7 @@ export default class SpeakerDetail {
       $('#title').html(title)
       $('#img').html(image)
       $('#links').html(link.join("</li>"))
-      $('#presentations').html(presentation)
+      $('#description').html(description)
 
     })
   }
